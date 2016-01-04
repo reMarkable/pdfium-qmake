0923b2c6ce2c8d7416080c31402d3f47976c1d7b
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-01-04 10:40:47 +0100

    epframebuffer: add a comment with approximate measured timing, and highlight waveform to enum

--- qsgepaper/epframebuffer.h
+++ qsgepaper/epframebuffer.h
@@ -11,11 +11,25 @@ class EPFrameBuffer : public QObject
 public:
     static EPFrameBuffer *instance();
 
+    //   Waveforms:
+    // 0: ~780ms, initialize to white
+    // 1: ~170ms, mono, no flashing
+    // 2: ~460ms, 15 grayscale, flashing all pixels white -> black pixels
+    // 3: ~460ms, 15, grayscale, flashing all pixels white -> black pixels
+    // 4: ~460ms, 4 grayscale, flashing all pixels white -> black pixels
+    // 5: ~460ms, 4 grayscale, flashing all pixels white -> black pixels
+    // 6: ~135ms, no flashing
+    // 7: ~300ms, white, 2 gray, 1 black, no flashing, stuck black?
+    // 8: ~435ms, fast flashing, all gray, highlight, several grays, lowest color - 1 gets stuck
+    // 9: ~2365ms, lots of flashing, delta something?
+
     enum Waveform {
         Initialize = 0,
         Mono = 1,
-        Grayscale = 2
+        Grayscale = 2,
+        Highlight = 8
     };
+
     enum UpdateMode {
         PartialUpdate = 0x0,
         FullUpdate = 0x1
