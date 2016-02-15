018eb732a1079deefa2a266441bd9b02081dca47
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-02-15 14:45:10 +0100

    debug: add debug helper class

--- app/debug.h
+++ app/debug.h
@@ -1,4 +1,36 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#ifdef DEBUG_THIS
+
+#include <QDebug>
+#include <QElapsedTimer>
+
+static int debugHelper_indent = 0;
+struct DebugHelperClass {
+    DebugHelperClass(QString funcInfo) : m_funcInfo(funcInfo) {
+        //qDebug().noquote() << QByteArray(debugHelper_indent, ' ') << '+' << m_funcInfo;
+        m_timer.start();
+        debugHelper_indent++;
+    }
+
+    ~DebugHelperClass() {
+        debugHelper_indent--;
+        if (m_timer.elapsed() > 10) {
+            qDebug().noquote() << QByteArray(debugHelper_indent, ' ') << '-' << m_funcInfo << m_timer.elapsed() << "ms";
+        }
+    }
+
+    QString m_funcInfo;
+    QElapsedTimer m_timer;
+};
+
+#define DEBUG_BLOCK DebugHelperClass debugHelper__Block(Q_FUNC_INFO);
+
+#else
+
+#define DEBUG_BLOCK
+
+#endif // DEBUG_THIS
+
 #endif // DEBUG_H
